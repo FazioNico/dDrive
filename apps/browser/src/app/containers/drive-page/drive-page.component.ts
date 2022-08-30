c36de08f0ccdfa22e823f7b64075d8afc855c548
FazioNico <contact@nicolasfazio.ch>
2022-08-30 17:18:01 +0200

    feat: testing tableland db

--- apps/browser/src/app/containers/drive-page/drive-page.component.ts
+++ apps/browser/src/app/containers/drive-page/drive-page.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AlertController, PopoverController, ToastController } from '@ionic/angular';
 import { BehaviorSubject, of } from 'rxjs';
 import { FilesOptionsListComponent } from '../../components/files-options-list/files-options-list.component';
+import { IPFSService } from '../../services/ipfs.service';
+import { TablelandService } from '../../services/tableland.service';
 
 @Component({
   selector: 'd-drive-drive-page',
@@ -24,13 +26,34 @@ export class DrivePageComponent  {
     private readonly _popCtrl: PopoverController,
     private readonly _toastCtrl: ToastController,
     private readonly _alertCtrl: AlertController,
+    private readonly _ipfsService: IPFSService,
+    private readonly _tablelandService: TablelandService
   ) {}
 
   async actions(type: string, payload?: any) {
     console.log('actions(): ', type);
     switch (true) {
-      case type === 'onFileChange':
+      case type === 'onFileChange': {
+        const file = payload.target.files[0];
+        if (!file) {
+          return;
+        }
+        // upload file to ipfs
+        const { cid } = await this._ipfsService.add(file);
+        console.log('cid: ', cid.toString());
+        // save file data to tableland
+        const { _id, hash } = await this._tablelandService.saveData('ddrive_files', {
+          name: file.name,
+          size: file.size,
+          type: file.type,
+          cid,
+          isFolder: false
+        });
+        console.log('_id: ', _id);
+        console.log('hash: ', hash);
+        
         break;
+      }  
       case type === 'navToFolderName':
         break;
       case type === 'reload':
@@ -43,6 +66,7 @@ export class DrivePageComponent  {
           throw new Error('openOptions(): payload is invalid');
         }
         await this.openOptions(event, item);
+        break;
       }
     }
   }
--- apps/browser/src/app/services/tableland.service.ts
+++ apps/browser/src/app/services/tableland.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from "@angular/core";
-import { connect, Connection, ConnectOptions, resultsToObjects } from '@tableland/sdk';
+import { connect, Connection, ConnectOptions, resultsToObjects, SUPPORTED_CHAINS } from '@tableland/sdk';
 import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class TablelandService {
     private _db!: Connection;
+
+    constructor() {
+      this.connect()
+          .then(async () => {
+            const tables = await this._db.list();
+            console.log('[INFO] Existing Tableland Tables: ', tables);
+          });
+    }
 
     async connect(options: ConnectOptions = { network: "testnet", chain: "polygon-mumbai" }) {
       try {
@@ -20,23 +28,37 @@ export class TablelandService {
       if (!this._db) {
         await this.connect();
       }
-      const tables = await this._db.list();
-      const collectionTable = tables.find(t => t.name === collection);
-      if (!collectionTable) {
-        throw new Error(`Collection ${collection} not found`);
-      }
-      const { _id } = data||uuidv4();
+      // formating collection name
+      collection = collection.toLowerCase().replace(/ /g, '_').replace(/-/g, '_');
+      const { _id = uuidv4()} = data;
       const keys = Object.keys(data);
-      const values = Object.values(data);
+      const values = Object.values(data).map(v => String(v));
       // add the _id if not present
       if (!keys.includes('_id')) {
         keys.push('_id');
         values.push(_id);
       }
+      // request db
+      const tables = await this._db.list();
+      console.log('[INFO] tables', tables); 
+      let collectionTable = Object.values(tables).find(t => t.name.includes(collection));
+      console.log('[INFO] collectionTable', collectionTable);      
+      if (!collectionTable) {
+        const q = `${keys.map((key) => `${key} text`).join(', ')}, _id text`;
+        console.log('[INFO] Creating table:', collection, q);        
+        const result = await this._db.create(q, {prefix: collection});
+        console.log('[INFO] Table created:', result);  
+        collectionTable = {
+          name: result.name||collection,
+          controller: 'current_user_address',
+          structure: result.txnHash,
+          createdAt: new Date().toISOString(),
+        }     
+      }
       // INSERT data into table
-      const insertRes = await this._db.write(
-        `INSERT INTO ${collection} (${keys.toString()}) VALUES (${values.toString()});`
-      );
+      const q = `INSERT INTO ${collectionTable.name} (${keys.toString()}) VALUES (${values.map(v => (`'${v}'`)).toString()})`;
+      console.log('[INFO] Inserting data:', q);      
+      const insertRes = await this._db.write(q);
       // extract the `hash` from the result
       const { hash } = insertRes;
       // return the `hash` with `_id`
@@ -49,6 +71,9 @@ export class TablelandService {
       if (!this._db) {
         await this.connect();
       }
+      // formating collection name
+      collection = collection.toLowerCase().replace(/ /g, '_').replace(/-/g, '_');
+      // request db
       const tables = await this._db.list();
       const collectionTable = tables.find(t => t.name === collection);
       if (!collectionTable) {
@@ -75,6 +100,9 @@ export class TablelandService {
       if (!this._db) {
         await this.connect();
       }
+      // formating collection name
+      collection = collection.toLowerCase().replace(/ /g, '_').replace(/-/g, '_');
+      // request db
       const tables = await this._db.list();
       const collectionTable = tables.find(t => t.name === collection);
       if (!collectionTable) {
@@ -91,6 +119,9 @@ export class TablelandService {
       if (!this._db) {
         await this.connect();
       }
+      // formating collection name
+      collection = collection.toLowerCase().replace(/ /g, '_').replace(/-/g, '_');
+      // request db
       const tables = await this._db.list();
       const collectionTable = tables.find(t => t.name === collection);
       if (!collectionTable) {
@@ -104,4 +135,17 @@ export class TablelandService {
       return {hash};
     }
     
+    private _getValueType(values: any[],i: number){
+      const e = values[i];
+      const valuueType =  typeof e;
+      if (valuueType === 'string') {
+        return 'text';
+      } else if (valuueType === 'number') {
+        return 'integer';
+      } else if (valuueType === 'boolean') {
+        return 'text';
+      } else {
+        return 'text';
+      }
+    }
 }
