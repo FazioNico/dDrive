ffa865d7193f10d1cb2e49c6872592a01131b8b1
FazioNico <contact@nicolasfazio.ch>
2022-09-06 21:24:45 +0200

    feat: mannage accoutn and chain change

--- apps/browser/src/app/guards/auth.guard.ts
+++ apps/browser/src/app/guards/auth.guard.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
-import { providers } from 'ethers';
 import { DIDService } from '../services/did.service';
 import { CeramicService } from '../services/ceramic.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+
   constructor(
     private _router: Router,
     private _did: DIDService,
     private _ceramic: CeramicService
   ) {}
 
-  async canActivate(): Promise<boolean> {
-    const ethereum: providers.ExternalProvider = (window as any)?.ethereum;
+  async canActivate(
+  ): Promise<boolean> {
+    const ethereum = (window as any)?.ethereum;
     const did = await this._did.init(ethereum);
     if (!did) {
       this._router.navigate(['login']);
--- apps/browser/src/app/services/did.service.ts
+++ apps/browser/src/app/services/did.service.ts
@@ -11,27 +11,52 @@ export class DIDService {
   public readonly accountId$ = new BehaviorSubject(null as any);
   public readonly chainId$ = new BehaviorSubject(null as any);
 
-  async init(ethereumProvider: any) {
-    
+  async init(ethereumProvider: any) {    
+    if (this.did) {
+      console.log('[INFO] already authenticated');      
+      return this.did;
+    }
     // Request accounts from the Ethereum provider
-    const accounts = await ethereumProvider.request({
-      method: 'eth_requestAccounts',
-    })
-    .catch((err: any) => {
-      throw `Error during Web3 Authetication: ${err?.message||'Unknown error'}`;
-    });
+    // if (ethereumProvider.send) {
+    if (!ethereumProvider.request) {
+      throw 'No request function found';
+    }
+    const accounts = await ethereumProvider.request({ method: 'eth_requestAccounts' })
+      .catch((err: any) => {
+        throw `Error during Web3 Authetication: ${err?.message||'Unknown error'}`;
+      });
+    if (ethereumProvider.isMetaMask) {
+      (ethereumProvider as any).on('connect',(connectInfo: any) => {
+        // console.log('connectInfo', connectInfo);
+      });
+      (ethereumProvider as any).on('chainChanged', (newNetwork: string) => {
+        // When a Provider makes its initial connection, it emits a "network"
+        // event with a null oldNetwork along with the newNetwork. So, if the
+        // oldNetwork exists, it represents a changing network
+        if (newNetwork) {
+            window.location.href = '/login';
+        }
+      });
+      (ethereumProvider as any).on('accountsChanged', (e:string[]) => {
+        // Handle the new accounts, or lack thereof.
+        // "accounts" will always be an array, but it can be empty.
+        if (e) {
+          window.location.href = '/login';
+        }
+      });
+    }
     if ((accounts?.length||0) === 0) {
       throw 'No accounts found. Please unlock your Ethereum account, refresh the page and try again.';
     }
-    this.chainId$.next(ethereumProvider.chainId);
+    this.chainId$.next(ethereumProvider?.chainId);
     // Create an EthereumAuthProvider using the Ethereum provider and requested account
     const account: string = accounts[0];
-    const authProvider = new EthereumAuthProvider(ethereumProvider, account)
+    const authProvider = new EthereumAuthProvider(ethereumProvider, account);
     // Connect the created EthereumAuthProvider to the 3ID Connect instance so it can be used to
     // generate the authentication secret
     await this._threeID.connect(authProvider);
     this.accountId$.next(account);
-    console.log('[INFO] create DID');      
+    // console.log('[INFO] create DID');      
     this.did = new DID({
       // Get the DID provider from the 3ID Connect instance
       provider: this._threeID.getDidProvider(),
