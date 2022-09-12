20096180237451a7ad8c06b1ef053fc94835d8c8
FazioNico <contact@nicolasfazio.ch>
2022-09-12 13:13:16 +0200

    feat: basic nft api implememntaion

---
+++ apps/browser/src/app/containers/nft-page/nft-page.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NFTPageComponent } from './nft-page.component';
+
+describe('NFTPageComponent', () => {
+  let component: NFTPageComponent;
+  let fixture: ComponentFixture<NFTPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [NFTPageComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(NFTPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ apps/browser/src/app/services/nft.services.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import Moralis from 'moralis';
+import { EvmChain, EvmNft } from '@moralisweb3/evm-utils';
+import { MoralisCore } from '@moralisweb3/core';
+import { MoralisEvmApi } from '@moralisweb3/evm-api';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable()
+export class NFTService {
+  public readonly nfts$ = new BehaviorSubject([] as EvmNft[]);
+  private _core!: MoralisCore;
+  private _evmApi!: MoralisEvmApi;
+
+  async connect() {
+    this._core = MoralisCore.create();
+    this._evmApi = MoralisEvmApi.create(this._core);
+    this._core.registerModules([this._evmApi]);
+    await this._core.start({
+      apiKey:
+        'F4Nu9K9EQdH7f2LFNCDFJ17pLhCPjCN5lEpSkp54Wtp5XBhOcJCN6zR0LVqlPC1c',
+    });
+    console.log('[INFO] connected');
+  }
+
+  async getWalletNFTs(address: string, chain: EvmChain = EvmChain.MUMBAI) {
+    Moralis.start({
+      apiKey:
+        'F4Nu9K9EQdH7f2LFNCDFJ17pLhCPjCN5lEpSkp54Wtp5XBhOcJCN6zR0LVqlPC1c',
+    });
+    // const evmApi = this._core.getModule<MoralisEvmApi>(MoralisEvmApi.name);
+
+    // const module = this._provider.getModule<MoralisEvmApi>(MoralisEvmApi.name);
+    const response = await Moralis.EvmApi.nft.getWalletNFTs({
+      address,
+      chain,
+    });
+    return response.result;
+  }
+
+  async getWalletNFTsFromAllChain(address: string) {
+    // await this.connect();
+    const chains = [EvmChain.MUMBAI, EvmChain.ETHEREUM];
+    const nfts = await Promise.all(
+      chains.map(async (chain) => this.getWalletNFTs(address, chain))
+    ).then((nfts) => nfts.flat());
+    this.nfts$.next(nfts);
+    console.log('[INFO] nfts', nfts);    
+    return nfts;
+  }
+}
