6f6023a8188075f534e7770474b7e408170d2057
FazioNico <contact@nicolasfazio.ch>
2022-09-18 23:58:42 +0200

    refactor: set default chain with change option

--- apps/browser/src/app/services/lit.service.ts
+++ apps/browser/src/app/services/lit.service.ts
@@ -6,12 +6,14 @@ import { IAccessControlConditions } from '../interfaces/mediafile.interface';
 
 @Injectable()
 export class LitService {
-  public readonly chain = environment.defaultChain;
+  private readonly _chain = environment.defaultChain;
+  private _litNodeClient: any;
+  private _authSig!: any;
   public readonly standardContractType = '';
   public readonly contractAddress = '';
   public defaultAccessControls: IAccessControlConditions[] = [
     {
-      chain: this.chain,
+      chain: this._chain,
       contractAddress: this.contractAddress,
       standardContractType: this.standardContractType,
       method: 'eth_getBalance',
@@ -23,15 +25,8 @@ export class LitService {
     },
   ];
 
-  private _litNodeClient: any;
-  private _authSig!: any;
-
   constructor(private readonly _loadingCtrl: LoadingController) {
-    console.log('[INFO] ALL_LIT_CHAINS: ', LitJsSdk.ALL_LIT_CHAINS);
-    console.log(
-      '[INFO] LIT_AUTH_SIG_CHAIN_KEYS: ',
-      LitJsSdk.LIT_AUTH_SIG_CHAIN_KEYS
-    );
+    // console.log('[INFO] ALL_LIT_CHAINS: ', LitJsSdk.ALL_LIT_CHAINS);
   }
 
   private async _connect() {
@@ -49,7 +44,8 @@ export class LitService {
 
   async encrypt(
     file: File | Blob,
-    accessControlConditions: IAccessControlConditions[]
+    accessControlConditions: IAccessControlConditions[],
+    chain = this._chain
   ): Promise<{
     encryptedFile: Blob;
     encryptedSymmetricKey: string;
@@ -58,30 +54,17 @@ export class LitService {
       await this._connect();
     }
     if (!this._authSig) {
-      this._authSig = await this._getAuthSig();
+      this._authSig = await this._getAuthSig(chain);
     }
     const { encryptedFile, symmetricKey } = await LitJsSdk.encryptFile({
       file: file,
     });
-    // [
-    //   {
-    //     chain: this.chain,
-    //     contractAddress: this.contractAddress,
-    //     standardContractType: this.standardContractType,
-    //     method: 'eth_getBalance',
-    //     parameters: [':userAddress', 'latest'],
-    //     returnValueTest: {
-    //       comparator: '>=',
-    //       value: '0',  // 0.000001 ETH
-    //     },
-    //   },
-    // ];
 
     const encryptedSymmetricKey = await this._litNodeClient.saveEncryptionKey({
       accessControlConditions,
       symmetricKey,
       authSig: this._authSig,
-      chain: this.chain,
+      chain,
       permanent: false,
     });
     return {
@@ -96,7 +79,8 @@ export class LitService {
   async decrypt(
     encryptedFile: File | Blob,
     encryptedSymmetricKey: string,
-    accessControlConditions: IAccessControlConditions[]
+    accessControlConditions: IAccessControlConditions[],
+    chain = this._chain
   ): Promise<{ decryptedArrayBuffer: ArrayBuffer }> {
     if (!this._litNodeClient) {
       await this._connect();
@@ -104,7 +88,7 @@ export class LitService {
     console.log('>>>> decrypt rules: ', accessControlConditions);
 
     if (!this._authSig) {
-      this._authSig = await this._getAuthSig();
+      this._authSig = await this._getAuthSig(chain);
     }
     console.log(
       '[INFO] Message signed, try to get encryption key from LitNode'
@@ -112,7 +96,7 @@ export class LitService {
     const symmetricKey = await this._litNodeClient.getEncryptionKey({
       accessControlConditions,
       toDecrypt: encryptedSymmetricKey,
-      chain: this.chain,
+      chain,
       authSig: this._authSig,
     });
     console.log('[INFO] Encryption key retrieved, try to decrypt file');
@@ -131,7 +115,7 @@ export class LitService {
     console.log('[INFO] LitNode disconnected');
   }
 
-  private async _getAuthSig() {
+  private async _getAuthSig(chain: string) {
     // display loader for a better UX
     const loading = await this._loadingCtrl.create({
       message: `
@@ -146,7 +130,7 @@ export class LitService {
     console.log('befor sign auth message');
     await new Promise((resolve) => setTimeout(resolve, 3000));
     const authSig = await LitJsSdk.checkAndSignAuthMessage({
-      chain: this.chain,
+      chain,
     });
     console.log('after sign auth message');
     loading.dismiss();
