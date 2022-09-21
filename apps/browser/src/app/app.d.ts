30b0aafc8bee6eb7b7f42e9ad97c4b1a53a87553
FazioNico <contact@nicolasfazio.ch>
2022-09-21 13:06:56 +0200

    refactor: mv declaration

--- apps/browser/src/app/app.d.ts
+++ apps/browser/src/app/app.d.ts
@@ -1,3 +1,10 @@
 declare module 'lit-js-sdk';
 declare module '@lit-protocol/sdk-browser';
 declare module '@metamask/jazzicon';
+declare var process: {
+  env: {
+    NG_APP_ENV: string;
+    // Replace the line below with your environment variable for better type checking
+    [key: string]: any;
+  };
+};
